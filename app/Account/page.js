4aa2a4c5dc11bcc795557f107c3e087d10fa8696
Bryan <suprnewb@gmail.com>
2023-10-24 22:25:15 +1100

    Styling

--- app/Account/page.js
+++ app/Account/page.js
@@ -1,11 +1,13 @@
 export default function Page() {
   return (
-    <div className="account">
+    <div className="account m-10">
       <h4>My Account</h4>
-      <form>
+      <form className="m-5">
         <input type="text" placeholder="Username or email address" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <button type="submit" className="w-10 h-10 bg-blue-300">
+          Login
+        </button>
       </form>
     </div>
   );
