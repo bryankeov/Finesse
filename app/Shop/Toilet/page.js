fad746e3bccfedf87bde63dc48148a21121c4fe8
Bryan <suprnewb@gmail.com>
2023-11-05 12:04:22 +1100

    update dir

---
+++ app/Shop/Toilet/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Toilet() {
+  return <div>Toilet</div>;
+}
+
+export default Toilet;
--- app/Shop/page.js
+++ app/Shop/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="shop">
-      <Link href="/Range/Toilet">Bathroom Tissue Range</Link>
+      <Link href="/Toilet">Bathroom Tissue Range</Link>
       <Link>Facial Tissue Range</Link>
       <Link>Kitchen Towel Range</Link>
       <Link>Napkin Range</Link>
