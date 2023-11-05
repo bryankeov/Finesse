76e0dd84ee6f640b376b3b938ed8b581ccdc037a
Bryan <suprnewb@gmail.com>
2023-11-05 12:11:56 +1100

    directory update

---
+++ app/Shop/Facial/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Facial() {
+  return <div>Facial</div>;
+}
+
+export default Toilet;
--- app/Shop/page.js
+++ app/Shop/page.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-export default function Page() {
+export default function Shop() {
   return (
     <div className="shop">
       <Link href="/Toilet">Bathroom Tissue Range</Link>
-      <Link>Facial Tissue Range</Link>
-      <Link>Kitchen Towel Range</Link>
-      <Link>Napkin Range</Link>
+      <Link href="/Facial">Facial Tissue Range</Link>
+      <Link href="/Kitchen">Kitchen Towel Range</Link>
+      <Link href="/Napkin">Napkin Range</Link>
     </div>
   );
 }
